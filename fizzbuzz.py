f83cc933dbded98362d20825a0dcdffb71fca424
appfogtest8 <appfogtest8@mm.st>
2013-11-24 19:26:47 -0800

    init commit

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+eulogy = raw_input("? ")
+x = int(eulogy)
+print x
+
+
+for i in range(1, x):
+ if i % 15 == 0:
+ 	print("fizzbuzz")
+ elif i % 3 == 0:
+ 	print("fizz")
+ elif i % 5 == 0:
+ 	print("buzz")
+ else:
+ 	print(i)
